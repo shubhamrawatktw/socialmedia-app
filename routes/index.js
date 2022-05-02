cabf17828b748e3e6c31f83bee9fc95df0363969
shubham <shubhamrawatktw@gmail.com>
2022-05-02 19:22:54 +0530

    posts created

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 const homeController = require("../controllers/home_controller");
+const postController = require("../controllers/post_controller")
 // console.log("router loaded");
 
 router.get("/", homeController.home);
 router.use("/users", require("./users"));
+router.post("/create-post",postController.createPost)
 
 
 module.exports = router;
