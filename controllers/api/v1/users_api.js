013b97fcb4f67db92d5da2e77189efa6968f3dcd
shubham <shubhamrawatktw@gmail.com>
2022-05-08 14:10:14 +0530

    create-session using jwt

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,28 @@
+const User = require("../../../models/users")
+const jwt = require("jsonwebtoken")
+
+
+module.exports.createSession = async (req, res) => {
+ try {
+     let user = await User.findOne({email:req.body.email})
+
+     if (!user || user.password != req.body.password) {
+         return res.json(422,{
+             message:"Invalid Username/Password"
+         })
+     }
+
+     return res.json(200,{
+         message:"successful , here is your token keep it safe",
+         data:{
+             token:jwt.sign(user.toJSON(),"codeial",{expiresIn:10000})
+         }
+     })
+     
+ } catch (error) {
+    return res.json(500,{
+        message:"internal server error"
+    })
+ }
+
+  };
