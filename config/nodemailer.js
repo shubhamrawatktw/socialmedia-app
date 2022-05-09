e53670e62c7f2a2d4dfa936d229ed21bd6276299
shubham <shubhamrawatktw@gmail.com>
2022-05-09 21:05:22 +0530

    setting up nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,39 @@
+const nodemailer = require("nodemailer")
+const ejs = require("ejs")
+const path = require("path")
+
+
+
+let transporter = nodemailer.createTransport({
+    service : "gmail",
+    host:"smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: "shubhamrawatktw@gmail.com", 
+        pass: "sr@035711", 
+      },
+})
+
+
+let renderTemplate = (data,relativePath)=>{
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname,"../views/mailers",relativePath),
+        data,
+        (err,template)=>{
+            if (err) {
+                console.log("error in rendering template");
+                return
+            }
+            mailHTML = template;
+        }
+    )
+
+    return mailHTML;
+}
+
+module.exports = {
+    transporter:transporter,
+    renderTemplate:renderTemplate
+}
